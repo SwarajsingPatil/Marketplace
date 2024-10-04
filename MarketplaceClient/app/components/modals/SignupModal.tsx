cb930c300fe792fbcdc016c469536390b5a61467
Swarajsing Patil <swarajsingpatil@gmail.com>
2024-10-04 14:55:41 -0400

    adding signup errors

--- MarketplaceClient/app/components/modals/SignupModal.tsx
+++ MarketplaceClient/app/components/modals/SignupModal.tsx
@@ -1,25 +1,39 @@
 'use client'
 import Modal from "./Modal"
 import { useState } from "react"
+import { useRouter } from "next/navigation"
 import useSignupModal from "@/app/hooks/useSignUpModal"
 import CustomButton from "../forms/CustomButton"
 
 const SignupModal = () => {
-const signupModal = useSignupModal()
+    const router = useRouter()
+    const signupModal = useSignupModal()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
+    const [errors, setErrors] = useState<string[]>([])
 
     const content = (
         <>
             {/* <h2 className="mb-6 text-2xl">Welcome to marketplace, please log in</h2> */}
 
             <form className="space-y-4" action="">
-                <input placeholder="Your email address" type="email" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setEmail(e.target.value)} placeholder="Your email address" type="email" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
                 
-                <input placeholder="Your password" type="password" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
-                <input placeholder="Confirm password" type="password" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setPassword(e.target.value)} placeholder="Your password" type="password" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setPassword2(e.target.value)} placeholder="Confirm password" type="password" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
                 
-                <div className="p-5 text-red-700 rounded-xl opacity-80">
-                    yooo
-                </div>
+                {errors.map((error,index) => {
+                return(
+                    <div 
+                        key={`error_${index}`}
+                        className="p-5 text-red-700 font-bold rounded-xl opacity-80">
+                        {error}
+                    </div>
+                    )
+                })}
+
+                
 
                 <CustomButton
                 label="Log in" 
