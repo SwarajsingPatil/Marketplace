c01fdb1650fd2fc62967c53d4bacdc07db86beaa
Swarajsing Patil <swarajsingpatil@gmail.com>
2024-10-05 17:27:50 -0400

    added cookies on signup.tsx

--- MarketplaceClient/app/components/modals/SignupModal.tsx
+++ MarketplaceClient/app/components/modals/SignupModal.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation"
 import useSignupModal from "@/app/hooks/useSignUpModal"
 import CustomButton from "../forms/CustomButton"
 import apiService from "@/app/services/apiService"
+import { handleLogin } from "@/app/lib/actions"
 
 const SignupModal = () => {
     const router = useRouter()
@@ -24,7 +25,7 @@ const SignupModal = () => {
         const response = await apiService.post('/api/auth/register/', JSON.stringify(formData))
         
         if (response.access){
-
+            handleLogin(response.user.pk, response.access, response.refresh)
             signupModal.close()
 
             router.push('/')
