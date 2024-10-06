068598598386cbb8be5a1972e897d57d58305949
Swarajsing Patil <swarajsingpatil@gmail.com>
2024-10-06 09:37:11 -0400

    login functionality completed  both authentication working now. Logout remaining

--- MarketplaceClient/app/components/modals/LoginModal.tsx
+++ MarketplaceClient/app/components/modals/LoginModal.tsx
@@ -3,26 +3,65 @@ import Modal from "./Modal"
 import { useState } from "react"
 import useLoginModal from "@/app/hooks/useLoginModal"
 import CustomButton from "../forms/CustomButton"
+import { useRouter } from "next/navigation"
+
+import apiService from "@/app/services/apiService"
+import { handleLogin } from "@/app/lib/actions"
 
 const LoginModal = () => {
-const loginModal = useLoginModal()
+
+    const router = useRouter()
+    const loginModal = useLoginModal()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState<string[]>([])
+
+
+    const submitLogin = async() => {
+        const formData = {
+            email:email,
+            password:password
+        }
+
+        const response = await apiService.post('/api/auth/login/', JSON.stringify(formData) )
+        if (response.access){
+            handleLogin(response.user.pk, response.access, response.refresh)
+            loginModal.close()
+
+            router.push('/')
+        }else{
+            setErrors(response.non_field_errors)
+        }
+
+    }
+
 
     const content = (
         <>
             {/* <h2 className="mb-6 text-2xl">Welcome to marketplace, please log in</h2> */}
 
-            <form className="space-y-4" action="">
-                <input placeholder="Your email address" type="email" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
+            <form 
+            action={submitLogin}
+            className="space-y-4"
+            >
                 
-                <input placeholder="Your password" type="password" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
+                <input onChange={(e) => setEmail(e.target.value)}  placeholder="Your email address" type="email" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
                 
-                <div className="p-5 text-red-700 rounded-xl opacity-80">
-                    yooo
-                </div>
+                <input onChange={(e) => setPassword(e.target.value)} placeholder="Your password" type="password" className="w-full px-4 h-[54px] border border-gray-300 rounded-xl" />
+                
+                {errors.map((error,index) => {
+                return(
+                    <div 
+                        key={`error_${index}`}
+                        className="p-5 text-red-700 font-bold rounded-xl opacity-80">
+                        {error}
+                    </div>
+                    )
+                })}
 
                 <CustomButton
                 label="Log in" 
-                onClick={() => console.log('test')}
+                onClick={submitLogin}
                 />
             </form>
         </>
