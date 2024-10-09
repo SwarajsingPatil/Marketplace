50733a05185c45d88f92c8ae7a7c1308e58b4ca3
Swarajsing Patil <swarajsingpatil@gmail.com>
2024-10-09 16:46:50 -0400

    updated AddProperty modal. Work in Progress

--- MarketplaceClient/app/components/addproperty/Categories.tsx
+++ MarketplaceClient/app/components/addproperty/Categories.tsx
@@ -12,44 +12,52 @@ const Categories: React.FC<CategoriesProps> = ({
 }) => {
     return (
         <>
-        <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12" >
-        <div className="pb-4 flex flex-col items-center spce-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image 
-                src="/icn_category_beach.jpeg"
-                alt="Category - Beach"
-                width={20}
-                height={20}
-                />
-                <span className="text-xs">Beach</span>
+            <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12" >
+                <div 
+                    onClick={() => setCategory('Beach')}
+                    className={`pb-4 flex flex-col items-center spce-y-2 border-b-2 ${dataCategory=='Beach' ? 'border-gray-800':  'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`} >
+                    <Image 
+                    src="/icn_category_beach.jpeg"
+                    alt="Category - Beach"
+                    width={20}
+                    height={20}
+                    />
+                    <span className="text-xs">Beach</span>
+                </div>
+                <div 
+                    onClick={() => setCategory('Villas')}
+                    className={`pb-4 flex flex-col items-center spce-y-2 border-b-2 ${dataCategory=='Villas' ? 'border-gray-800':  'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`} >
+                    <Image 
+                    src="/icn_category_beach.jpeg"
+                    alt="Category - Villas"
+                    width={20}
+                    height={20}
+                    />
+                    <span className="text-xs">Villas</span>
+                </div>
+                <div 
+                    onClick={() => setCategory('Cabins')}
+                    className={`pb-4 flex flex-col items-center spce-y-2 border-b-2 ${dataCategory=='Cabins' ? 'border-gray-800':  'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`} >
+                    <Image 
+                    src="/icn_category_beach.jpeg"
+                    alt="Category - Cabins"
+                    width={20}
+                    height={20}
+                    />
+                    <span className="text-xs">Cabins</span>
+                </div>
+                <div 
+                    onClick={() => setCategory('Tiny homes')}
+                    className={`pb-4 flex flex-col items-center spce-y-2 border-b-2 ${dataCategory=='Tiny homes' ? 'border-gray-800':  'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`} >
+                    <Image 
+                    src="/icn_category_beach.jpeg"
+                    alt="Category - Tiny homes"
+                    width={20}
+                    height={20}
+                    />
+                    <span className="text-xs">Tiny homes</span>
+                </div>
             </div>
-            <div className="pb-4 flex flex-col items-center spce-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image 
-                src="/icn_category_beach.jpeg"
-                alt="Category - Villas"
-                width={20}
-                height={20}
-                />
-                <span className="text-xs">Villas</span>
-            </div>
-            <div className="pb-4 flex flex-col items-center spce-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image 
-                src="/icn_category_beach.jpeg"
-                alt="Category - Cabins"
-                width={20}
-                height={20}
-                />
-                <span className="text-xs">Cabins</span>
-            </div>
-            <div className="pb-4 flex flex-col items-center spce-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image 
-                src="/icn_category_beach.jpeg"
-                alt="Category - Tiny homes"
-                width={20}
-                height={20}
-                />
-                <span className="text-xs">Tiny homes</span>
-            </div>
-        </div>
         
         </>
     )
--- MarketplaceClient/app/components/modals/AddPropertyModal.tsx
+++ MarketplaceClient/app/components/modals/AddPropertyModal.tsx
@@ -15,7 +15,8 @@ const AddPropertyModal = () => {
 
     const [currentStep, setCurrentStep] = useState(1)
     const [dataCategory,setDataCategory] = useState('')
-
+    const [dataTitle, setDataTitle] = useState('')
+    const [dataDescription, setDataDescription] = useState('')
 
     const addPropertyModal = useAddPropertyModal()
 
@@ -40,8 +41,63 @@ const AddPropertyModal = () => {
                         onClick={() => setCurrentStep(2)}
                     />
                 </> 
+            ) : currentStep == 2 ? (
+                <>
+                    <h2 className='mb-6 text-2xl'> Describe your place</h2>
+
+                    <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Title</label>
+                            <input
+                                type='text'
+                                value={dataTitle}
+                                placeholder='Enter title'
+                                onChange={(e) => setDataTitle(e.target.value)}
+                                className='w-full p-4 shadow shadow-md border border-gray-600 rounded-md'
+                            />
+                        </div>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Description</label>
+                            <textarea
+                                value={dataDescription}
+                                placeholder='Provide some description about your property'
+                                onChange={(e) => setDataDescription(e.target.value)}
+                                className='w-full p-4 h-[200px] shadow shadow-md border border-gray-600 rounded-md'
+                            ></textarea>
+                        </div>
+                        
+                        <CustomButton
+                            label='Previous'
+                            className='mb-2 bg-black hover:bg-gray-800'
+                            onClick={() => setCurrentStep(1)}
+                        />
+
+                        <CustomButton
+                            label='Next'
+                            onClick={() => setCurrentStep(3)}
+                        />
+
+                    </div>
+
+                </>
+            ): currentStep==3 ?(
+                <>
+                    <h2 className='mb-6 text-2xl'>Details</h2>
+
+                    <CustomButton
+                        label='Previous'
+                        className='mb-2 bg-black hover:bg-gray-800'
+                        onClick={() => setCurrentStep(2)}
+                    />
+
+                    <CustomButton
+                        label='Next'
+                        onClick={() => setCurrentStep(4)}
+                    />
+                </>
             ) : (
-                <>step 2</>
+                <>
+                </>
             )}
 
         </>
